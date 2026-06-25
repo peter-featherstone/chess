d9f09ee8ce8014b2bc7b46193b51fab1c3a9bd8d
Peter Featherstone <fe@therst.one>
2026-06-24 21:17:17 -0500

    Use fyne to make a matching Go GUI to Rust

--- go/main.go
+++ go/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
+	a := app.New()
+	w := a.NewWindow("Peter Chess")
+
 	board := NewBoard()
 
+	w.SetContent(widget.NewLabel("Hello World!"))
+
+	content := ""
 	for _, square := range board.Squares {
-		fmt.Printf("Square: %d:%d", square.xPos, square.yPos)
+		content += fmt.Sprintf("Square: %d:%d", square.xPos, square.yPos)
 	}
+
+	w.SetContent(widget.NewLabel(content))
+	w.ShowAndRun()
 }
